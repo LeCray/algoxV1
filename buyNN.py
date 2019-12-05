3193cd689d8a6549e99276d5a005e87e64218be6
LeCray <mistercryptogrid@gmail.com>
2019-12-05 08:45:37 +0200

    Trying to run prediction

--- buyNN.py
+++ buyNN.py
@@ -1,38 +1,73 @@
 import csv
 import pandas as pd
 import plotly.express as px
-from numpy import loadtxt
+import numpy as np
 import urllib.request
 import threading
 import pickle
 
+#import keras
+import tensorflow
+from tensorflow.keras import backend
 
-dataLocation = "C:/Users/Thembi/AppData/Roaming/MetaQuotes/Terminal/1DAFD9A7C67DC84FE37EAA1FC1E5CF75/tester/files/DataVehicle/dataVehicle.csv"
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+#import pickle
+import h5py
 
+mql_to_python_data = "C:/Users/Thembi/AppData/Roaming/MetaQuotes/Terminal/1DAFD9A7C67DC84FE37EAA1FC1E5CF75/tester/files/Data/MqlPython.csv"
+python_to_mql_data = "C:/Users/Thembi/AppData/Roaming/MetaQuotes/Terminal/1DAFD9A7C67DC84FE37EAA1FC1E5CF75/tester/files/Data/PythonMql.csv"
 # load the model from disk
-model = pickle.load(open('finalized_model.sav', 'rb'))
+#model = pickle.load(open('finalized_model.sav', 'rb'))
+#loaded_model = tensorflow.keras.models.load_model('buyNN.h5')
 
-index = 0
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
 
 def run_retriever():
     threading.Timer(1.0, run_retriever).start()
-    data = loadtxt(dataLocation, delimiter=',')
+    data = np.loadtxt(mql_to_python_data, delimiter=',')
+    predictionIndex = 0
 
-    if index != data[0,0]:
-        index = data[0,0] #Updating the index
+    featuresIndex = data[0]
+    print("Features Index: ", featuresIndex)
+
+    if featuresIndex > predictionIndex:
 
         print("Making prediction...")
 
-        X = data[0, 1:5]
-        y = model.predict_classes(X)
+        X = np.array(data[1:5])
+        print(X)
+
+
+        y = loaded_model.predict_classes(X)
+        print("Prediction: ", y)
+
+        predictionIndex = data[0] #Updating the index to match last features index from mql-server
 
         if y == 1:
-            data[0, 0] = "BUY"
-            numpy.savetxt(dataLocation, data)
+            predictionData[0] = predictionIndex
+            predictionData[1] = 1
+            np.savetxt(python_to_mql_data, predictionData)
+            print("Buy prediction sent to mql-server: 1")
         else:
-            print("DON'T BUY")
+            predictionData[0] = predictionIndex
+            predictionData[1] = 0
+            np.savetxt(python_to_mql_data, predictionData)
+            print("Buy prediction sent to mql-server: 0")
+
     else:
         print("RETRYING...")
+        run_retriever()
 
 
 run_retriever()
--- model.py
+++ model.py
@@ -5,8 +5,11 @@ import plotly.express as px
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.models import model_from_json
 
 import pickle
+
+import h5py
 
 
 dataset = loadtxt('Data/BuyData.csv', delimiter=',') #10357
@@ -38,8 +41,18 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 model.fit(X, y, epochs=150, batch_size=10)
 
 # save the model to disk
-filename = 'finalized_model.sav'
-pickle.dump(model, open(filename, 'wb'))
+#filename = 'finalized_model.sav'
+#model.save("buyNN.h5")
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+#pickle.dump(model, open(filename, 'wb'))
 
 # evaluate the keras model
 _, accuracy = model.evaluate(A, b)
